7904893af4f8681395fc5fca81c17d596bf9fc0d
Loic Dupong <loic@dupong.be>
2025-06-05 12:08:39 +0200

    Interface done - Need to do btns + score

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ const btnTestInterpol = document.querySelector('.btn--interpol');
 const btnTestRanUser = document.querySelector('.btn--random-user');
 const displayHTML = document.querySelector('.wrapper__results');
 let cleanRDI;
-let interpolData
+let interpolData;
+let interpolImg;
+let userData;
 
-// https://ws-public.interpol.int/notices/v1/red/${id}/images/${._embedded.images[0]}
+// == Fetch Interpol API Data of one data
 async function getInterpolAPI(id) {
     try {
         const response = await fetch(`https://ws-public.interpol.int/notices/v1/red/${id}`);
@@ -15,6 +17,19 @@ async function getInterpolAPI(id) {
     }
 }
 
+// == Fetch Interpol API Image based on a random data
+async function getInterpolImg(id) {
+    try {
+        const response = await fetch(`https://ws-public.interpol.int/notices/v1/red/${id}/images/`);
+        const data = await response.json();
+        interpolImg = data._embedded.images[0].picture_id;
+        return interpolImg
+    } catch (error) {
+       console.log("Error: ", error);
+    }
+}
+
+// == Fetch Interpol API for 100 data and pick a random one
 async function getRandomInterpolIdAPI() {
     try {
         const response = await fetch(`https://ws-public.interpol.int/notices/v1/red?resultPerPage=100`);
@@ -31,43 +46,80 @@ async function getRandomInterpolIdAPI() {
     }
 }
 
+// == Display Data of Interpol
 function displayInterpol(data, dataImg) {
     displayHTML.innerHTML = "";
+    let nationality;
+    data.nationalities ? nationality = data.nationalities.join(', ') : "";
+
+
     displayHTML.innerHTML += `
     <div class="single__container">
-        <div class="single__img"><img src="https://ws-public.interpol.int/notices/v1/red/${dataImg}/images/" alt=""></div>
+        <div class="single__img"><img src="https://ws-public.interpol.int/notices/v1/red/${dataImg}/images/${interpolImg}" alt="picture interpole"></div>
         <div class="single__infos">
             <div class="single__name">${data.name} ${(data.forename).toLowerCase()}</div>
-            <div class="single__sex">${data.sex_id}</div>
-            <div class="single__nationality">${data.nationalities.join(', ')}</div>
-            <div class="single__warrant">${data.arrest_warrants.map(g => g.charge).join(' | ')}</div>
+            <div class="single__sex"><span class="bold">Gender:</span> ${data.sex_id}</div>
+            <div class="single__nationality"><span class="bold">Nationalities:</span> ${data.nationalities.join(', ')}</div>
+            <div class="single__warrant">
+                <h3>Arrest Warrant:</h3>
+                <p>${data.arrest_warrants.map(g => g.charge).join(' | ')}</p>
+            </div>
         </div>
     </div>
     `
 }
 
-
-async function getRandomUserAPI(amount = 50) {
+// == Fetch a random fake user API
+async function getRandomUserAPI() {
     try {
-        const response = await fetch(`https://randomuser.me/api/?inc=gender,name,nat,picture&results=${amount}`)
+        const response = await fetch(`https://randomuser.me/api/?inc=gender,name,nat,picture`)
         const data = await response.json();
-        console.log(data.results);
-        return (data.results)
+        userData = data.results[0]
+        console.log(userData);
+        
+        return (userData)
     
     } catch (error) {
         console.log('Error: ', error);
     }
 }
 
+// == Display a Random fake user
+function displayRandomUser(data) {
+    displayHTML.innerHTML = "";
+    displayHTML.innerHTML += `
+    <div class="single__container">
+        <div class="single__img single__img--user"><img src="${data.picture.large}" alt="picture large"></div>
+        <div class="single__infos">
+            <div class="single__name">${(data.name.last).toUpperCase()} ${data.name.first}</div>
+            <div class="single__sex"><span class="bold">Gender:</span> ${data.gender}</div>
+            <div class="single__nationality"><span class="bold">Nationalities:</span> ${data.nat}</div>
+        </div>
+    </div>
+    `
+}
 
+async function Init() {
+    const id = await getRandomInterpolIdAPI(); 
+    await getInterpolAPI(id);
+    await getInterpolImg(id);
+    await getRandomUserAPI();
+    const functionsArray = [displayRandomUser(userData),displayInterpol(interpolData, cleanRDI)]
+
+    functionsArray[Math.floor(Math.random() * functionsArray.length)];
+}
+
+Init();
 
 btnTestInterpol.addEventListener('click', async (e) => {
     const id = await getRandomInterpolIdAPI(); 
     await getInterpolAPI(id);
+    await getInterpolImg(id);
     displayInterpol(interpolData, cleanRDI);
 });
 
 
 btnTestRanUser.addEventListener('click', async (e)=>{
     await getRandomUserAPI();
+    displayRandomUser(userData);
 })
